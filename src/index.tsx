b5e16464d5335b29426244f76f27b014a6131589
cynthiacodes <cjnkstudy01@hotmail.com>
2023-07-14 00:33:47 +0100

    added YeApp function component

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { YeApp } from "./YeApp";
+
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <YeApp />
   </React.StrictMode>,
   document.getElementById("root")
 );
